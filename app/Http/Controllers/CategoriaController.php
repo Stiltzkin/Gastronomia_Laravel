c38cdb5dfcff7ceac3f50363044171d38909e4b9
Andrey Kawaguchi <andrey.kawaguchi@opet.edu.br>
2018-02-20 13:03:58 -0300

    20/02/2018

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
+    public function __construct()
+    {
+        header('Access-Control-Allow-Origin: *');
+    }
     /**
      * Display a listing of the response.
      *
